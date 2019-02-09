3e5a5660852d93e3b25de0305cad5fc710053f7d
Antisocialamp <chatziiol@gmail.com>
2019-02-09 17:21:57 +0200

    Solved Expert Enough, new chapter

---
+++ datastructs/bitmask/bitmask.md
@@ -0,0 +1,4 @@
+# Bitmask Folder
+
+Fun exercises completed in order for me to hone my bitmasking skills.
+**Tedious** ones.
---
+++ datastructs/rails.cpp
@@ -0,0 +1,14 @@
+// Problem : U-Va ID: 514 - Rails
+
+#include <cstdio>
+
+int main () {
+	// Input will consist of multiple lines
+	
+	// Case related variables
+	int numberOfCoaches;
+
+	while ( scanf("%d", &numberOfCoaches ) == 1 ){
+		// if numberOfCoaches is equal to 0, the input has finished
+		if ( numberOfCoaches == 0) return 0;
+		
